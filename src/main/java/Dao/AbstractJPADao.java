375f80605fa6d9e25ad42fad8997b7e365905e5c
ali.rostamian <alibalii2000@gmail.com>
2020-12-29 03:11:28 +0330

    creating generic

--- src/main/java/Dao/AbstractJPADao.java
+++ src/main/java/Dao/AbstractJPADao.java
@@ -1,4 +1,32 @@
 package Dao;
 
-public abstract class AbstractJPADao {
+import javax.persistence.EntityManager;
+import javax.swing.border.AbstractBorder;
+
+public abstract class AbstractJPADao<T,U>
+{
+    private EntityManager entityManager;
+    private Class<T> entityClass;
+    public AbstractJPADao(EntityManager entityManager,Class<T> entityClass)
+    {
+        this.entityManager=entityManager;
+        this.entityClass=entityClass;
+    }
+    public void save (T entity)
+    {
+        entityManager.persist(entity);
+    }
+
+    public T load(U id)
+    {
+        return  entityManager.find(entityClass,id);
+    }
+    public void delete(T entity)
+    {
+        entityManager.remove(entity);
+    }
+    public void update(T entity)
+    {
+        entityManager.merge(entity);
+    }
 }
