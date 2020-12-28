8bdc632a8a287c3d1b5ac61035a99382fceb732b
ali.rostamian <alibalii2000@gmail.com>
2020-12-29 03:13:48 +0330

    completing addressdao class

--- src/main/java/Dao/AddressDao.java
+++ src/main/java/Dao/AddressDao.java
@@ -1,4 +1,13 @@
 package Dao;
 
-public class AddressDao {
+import Entities.Address;
+
+import javax.persistence.EntityManager;
+
+public class AddressDao extends AbstractJPADao<Address,Integer>
+{
+
+    public AddressDao(EntityManager entityManager, Class<Address> entityClass) {
+        super(entityManager, entityClass);
+    }
 }
